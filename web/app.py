74a6bb933b3dc86fb3d9ede20e79b4666dedb794
soundarzozm <soundar.murugan1@learner.manipal.edu>
2021-04-20 20:22:41 +0530

    password verification rectified

--- web/app.py
+++ web/app.py
@@ -13,10 +13,28 @@ db = client.SimilarityDB
 users = db["Users"]
 
 def UserExists(username):
-    if users.find({"username": username}).count() == 0:
+    if users.find({"username": username}).count() >= 1:
+        return True
+    return False
+
+def VerifyPassword(username, password):
+    if not UserExists(username):
         return False
-    else:
-        return True    
+
+    hashed_pw = users.find({
+        "username": username
+    })[0]["password"]
+
+    if bcrypt.checkpw(hashed_pw, password):
+        return True
+    return False
+
+def CountTokens(username):
+    num_tokens = users.find({
+        "username": username
+    })[0]["tokens"]
+
+    return num_tokens
 
 class Register(Resource):
     def post(self):
@@ -46,4 +64,108 @@ class Register(Resource):
             "msg": "success"
         }
 
-        return jsonify(retJson)        
+        return jsonify(retJson)
+
+class Detect(Resource):
+    def post(self):
+
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+        text1 = postedData["text1"]
+        text2 = postedData["text2"]
+
+        if UserExists(username) == False:
+            retJson = {
+                "status": 301,
+                "msg": "username not found"
+            }
+            return jsonify(retJson)
+
+        if VerifyPassword(username, password) == False:
+            retJson = {
+                "status": 302,
+                "msg": "incorrect password"
+            }
+            return jsonify(retJson)
+
+        num_tokens = CountTokens(username)
+
+        if num_tokens < 1:
+            retJson = {
+                "status": 303,
+                "msg": "tokens exhausted"
+            }
+            return jsonify(retJson)
+
+        model = spacy.load("en_core_web_sm")
+
+        text1 = model(text1)
+        text2 = model(text2)
+
+        ratio = text1.similarity(text2)
+
+        retJson = {
+            "status": 200,
+            "similarity": ratio,
+            "msg": "similarity score calculated"
+        }
+
+        users.update({
+            "username": username
+        },{
+            "$set":{
+                "tokens": num_tokens - 1
+            }
+        })
+
+        return jsonify(retJson)
+
+class Refill(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        adminPw = postedData["adminPw"]
+        refillAmt = postedData["refill"]
+
+        if UserExists(username) == False:
+            retJson = {
+                "status": 301,
+                "msg": "username not found"
+            }
+            return jsonify(retJson)
+
+        correct_pw = "abc123"
+
+        if adminPw != correct_pw:
+            retJson = {
+                "status": 304,
+                "msg": "invalid admin password"
+            }    
+            return jsonify(retJson)
+
+        num_tokens = CountTokens(username)
+
+        users.update({
+            "username": username
+        },{
+            "$set":{
+                "tokens": num_tokens + refillAmt
+            }
+        })
+
+        retJson = {
+            "status": 200,
+            "msg": "success"
+        }
+
+        return jsonify(retJson)
+
+api.add_resource(Register, "/register")
+api.add_resource(Detect, "/detect")
+api.add_resource(Refill, "/refill")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
